d0e9f4ec87abaef6ea28d3341185025bda0bc629
ShamusDuffey <shamusduffey@gmail.com>
2025-01-06 14:48:46 -0500

    Add files via upload

---
+++ Signin.js
@@ -0,0 +1,42 @@
+const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
+const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
+
+async function signUp(email, password) {
+    const { data, error } = await sb.auth.signUp({ email: email, password: password });
+    console.log("Signup Response:", data, error); // Log the response for debugging
+    if (error) {
+        alert("There was an issue signing you up: " + error.message);
+    } else {
+        alert("Welcome! You've been signed up! Just confirm your identity in your email to gain access to your account.");
+    }
+}
+
+async function signin(email, password) {
+    const { data, error } = await sb.auth.signInWithPassword({ email: email, password: password });
+    if (error) {
+        alert("There was an issue signing you in: " + error.message);
+    } else {
+        alert(`Welcome back, ${data.user.email}`);
+        document.getElementById("signin-form").style.display = "none";
+        document.getElementById("signup-form").style.display = "none";
+        window.location.href = "http://localhost:8000/prototype.html"
+    }
+}
+
+document.getElementById("signup-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+    const email = document.getElementById("signup-email").value;
+    const password = document.getElementById("signup-password").value;
+
+    await signUp(email, password);
+});
+
+document.getElementById("signin-form").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    await signin(email, password);
+});
