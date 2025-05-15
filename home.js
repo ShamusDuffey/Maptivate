868f86f49b9f5c8f5e1aeaf741246d04f3858956
ShamusDuffey <shamusduffey@gmail.com>
2025-05-15 15:39:34 -0400

    I deleted too much and I think this should fix it.

--- home.js
+++ home.js
@@ -1,4 +1,4 @@
-SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
+const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
 const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
 
@@ -29,4 +29,14 @@ createNewLayer.addEventListener('click', async() =>
         	alert('Data inserted successfully!');
       	}
 }
-
+var map = L.map('map').setView([42.63583, -71.314167], 14);
+L.tileLayer(
+	'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=a5ic5yAL7H7RtI0ALklW',
+	{
+		attribution:
+			'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>' +
+			' <a href="https://www.openstreetmap.org/copyright" target="_blank">' +
+			'&copy; OpenStreetMap contributors</a>',
+	}
+).addTo(map);
+L.marker([42.6431, -71.336262]).addTo(map);
