28e0edd8ecb854943af25a5a10eb04e23562f08f
ShamusDuffey <shamusduffey@gmail.com>
2025-05-17 18:57:26 -0400

    Wrapped the js in a window event listener

--- home.js
+++ home.js
@@ -2,6 +2,7 @@ const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
 const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
 let user_has_already_made_a_layer='f';
+window.addEventListener('DOMContentLoaded',()=>{
 createNewLayer.addEventListener('click', async() =>
 {
 	const layerNameInput=document.getElementById('layerNameInput');
@@ -55,3 +56,4 @@ map.on('click', async(e)=>
 		</div>;
 	new_pin.bindPopup(popupContent).openPopup();
 }
+});
