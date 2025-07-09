64ea1498f7f99bcc60ff3ad8352215e638da367f
ShamusDuffey <shamusduffey@gmail.com>
2025-07-08 22:35:33 -0400

    await can't be used gloablly (line 4)

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
 const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
-const {data: {session}, error: sessionError}=await sb.auth.getSession();
+const {data: {session}, error: sessionError}=sb.auth.getSession();
 if(sessionError) console.log(sessionError.message);
 let working_layer_ids=[null, null, null, null];
 let selected_layer_ids=[null, null, null, null];
