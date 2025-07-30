06ffb9e0765ad683d6481f071eb4c3de95942d42
ShamusDuffey <shamusduffey@gmail.com>
2025-07-30 03:48:52 -0400

    Add files via upload

--- GITusers.js
+++ GITusers.js
@@ -1,4 +1,4 @@
-import {loadPin, getUser, checkSession} from 'GIThome.js';
+import {loadPin, getUser, checkSession} from './GIThome.js';
 const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
 let session;
