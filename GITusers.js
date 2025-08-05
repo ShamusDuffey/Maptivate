52080d103b150264aa9259abf39b25b6875e4807
ShamusDuffey <shamusduffey@gmail.com>
2025-08-04 23:01:53 -0400

    fixed a syntax error on line 5

--- GITusers.js
+++ GITusers.js
@@ -1,9 +1,9 @@
 import {loadPin, getUser, checkSession} from './GIThome.js';
 const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
+const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
 const userId=localStorage.getItem("selectedUserId");
 let USER=sb.from("Users").select("*").eq("user_id", userId).single();
-document.getElementById=("usernameHeader").textContent=USER.display_name;
 const sb = supabase.createClient(SUPABASE_URL, supabaseKey);
 var map = L.map('map').setView([42.63583, -71.314167], 14);
 L.tileLayer(
