74b7e608ef0afc00adfd56d49e5e34265d8e1908
ShamusDuffey <shamusduffey@gmail.com>
2025-08-13 00:37:55 -0400

    Add files via upload

--- GITusers.js
+++ GITusers.js
@@ -1,5 +1,15 @@
 const SUPABASE_URL = 'https://tckolgmxbedfuytfkudh.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRja29sZ214YmVkZnV5dGZrdWRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzQ5MjY3MjMsImV4cCI6MjA1MDUwMjcyM30.FEemUUeRDJwT8s98mY2sZa0xwlh72EJQlzO7Kxa2uIA';
+var map = L.map('map').setView([42.63583, -71.314167], 14);
+L.tileLayer(
+	'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=a5ic5yAL7H7RtI0ALklW',
+        {
+                attribution:
+                        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>' +
+                        ' <a href="https://www.openstreetmap.org/copyright" target="_blank">' +
+                        '&copy; OpenStreetMap contributors</a>',
+        }
+).addTo(map);
 window.addEventListener('DOMContentLoaded',()=>{
 (async()=>
 {
@@ -9,16 +19,6 @@ const {data: USER, error: userFetchError}=await sb.from("Users").select("*").eq(
 if(userFetchError)
 	console.error(userFetchError.message);
 usernameHeader.textContent=USER.display_name;
-var map = L.map('map').setView([42.63583, -71.314167], 14);
-L.tileLayer(
-        'https://api.maptiler.com/maps/streets-v2/{z}/{x}/{y}.png?key=a5ic5yAL7H7RtI0ALklW',
-        {
-                attribution:
-                        '<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a>' +
-                        ' <a href="https://www.openstreetmap.org/copyright" target="_blank">' +
-                        '&copy; OpenStreetMap contributors</a>',
-        }
-).addTo(map);
 const {data: usersPins, count: postCount, error: pinsError}=await sb.from("Pin Posts").select('*', {count: "exact"}).eq("creator_id", USER.user_id);
 if(pinsError)
 {
